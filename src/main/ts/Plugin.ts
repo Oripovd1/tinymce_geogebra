cca3ece7fadaf1a041002973e10c4f088f0518d0
oripovd1 <oripovd1@gmail.com>
2024-04-07 23:15:38 +0200

    geogebra container size updated

--- src/main/ts/Plugin.ts
+++ src/main/ts/Plugin.ts
@@ -58,11 +58,13 @@ const setup = (editor: Editor, url: string): void => {
         api.close();
       },
     });
+    const vw = window.innerWidth;
+    const vh = window.innerHeight;
     const parameters = {
       id: "ggbApplet",
       appName: "classic",
-      width: 1150,
-      height: 500,
+      width: vw * 0.9 - 32,
+      height: vh * 0.9 - 128,
       showToolBar: true,
       borderColor: null,
       showMenuBar: true,
